c35ba2a4f3dbb7be98aeb2dbab0e0cd8532fe902
instaDev-mayuri <mayuri@instaacoders.com>
2025-04-04 18:02:02 +0530

    next prev btn functinlity done

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -12,6 +12,9 @@ const Pagination = ({
 }) =>{
     const router = useRouter();
 
+    const hasPrev = ITEM_PER_PAGE * (page - 1) > 0;
+    const hasNewxt = ITEM_PER_PAGE * (page - 1 ) + ITEM_PER_PAGE < count;
+
     const changePage = ( newPage : Number) => {
         const params = new URLSearchParams(window.location.search);
         params.set("page", newPage.toString());
@@ -19,7 +22,10 @@ const Pagination = ({
     }
     return (
         <div className="p-4 flex items-center justify-between text-gray-500">
-            <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Prev</button>
+            <button disabled={!hasPrev} className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+            onClick={()=>{
+                changePage(page-1)
+            }}>Prev</button>
             <div className="flex items-center gap-2 text-sm">
                 {Array.from(
                     {length : Math.ceil(count / ITEM_PER_PAGE) },
@@ -32,7 +38,11 @@ const Pagination = ({
                     }
                 )}
             </div>
-            <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Next</button>
+            <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={!hasNewxt}
+            onClick={()=>{
+                changePage(page+1)
+            }}>Next</button>
         </div>
     )
 }
